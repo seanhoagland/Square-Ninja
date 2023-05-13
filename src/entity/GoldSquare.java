e691e129b4040d031c0da56b3e2389e384fc623a
EthanChes <91499794+EthanChes@users.noreply.github.com>
2023-05-12 20:09:31 -0700

    Add files via upload

---
+++ src/entity/GoldSquare.java
@@ -0,0 +1,35 @@
+package entity;
+
+import GameManagement.GameManager;
+import graphics.Animation;
+import graphics.Camera;
+import graphics.Window;
+import world.World;
+
+public class GoldSquare extends Entity {
+    public static final int ANIM_SIZE = 1;
+    public static final int ANIM_IDLE = 0;
+
+
+
+    public GoldSquare(Transform transform) {
+        super(ANIM_SIZE, transform);
+        setAnimation(ANIM_IDLE, new Animation(1, 1, "Gold"));
+    }
+
+    @Override
+    public void update(float delta, Window window, Camera camera, World world) {
+        MoveDownScreen(delta, speed);
+    }
+
+    /** Destroys Square When Clicked On */
+    @Override
+    public void DestroySquare(GameManager gameManager) {
+        gameManager.score += 100; //adds to score
+    }
+
+    @Override
+    public void NoClickDestroy(GameManager gameManager) {
+
+    }
+}
---
+++ src/gameplay/Click.java
@@ -0,0 +1,44 @@
+package gameplay;
+
+import graphics.Camera;
+import org.joml.Vector2f;
+import org.lwjgl.BufferUtils;
+import java.nio.DoubleBuffer;
+
+import static org.lwjgl.glfw.GLFW.*;
+
+public class Click {
+    public static Vector2f position;
+    public static boolean clicked;
+
+    //sets cursor position and if mouse is clicked
+    public static void update(long window) {
+        clicked = checkClick(window);
+
+        DoubleBuffer posX = BufferUtils.createDoubleBuffer(1);
+        DoubleBuffer posY = BufferUtils.createDoubleBuffer(1);
+        glfwGetCursorPos(window, posX, posY);
+        position = new Vector2f( (float) posX.get(0), (float) posY.get(0) );
+    }
+
+    //checks if mouse is clicked
+    private static boolean checkClick(long window) {
+        int mouseState = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
+        if (mouseState == GLFW_PRESS)
+            return true;
+        return false;
+    }
+
+    //get position of mouse click
+    public static Vector2f ConvertClickPositionToRealCoordinates(Camera camera, Vector2f input, float scale) {
+        // Determine Top Left of Screen
+        float x = -camera.getPosition().x - camera.getWidth() / 2;
+        float y = -camera.getPosition().y + camera.getHeight() / 2;
+
+        // Combine With
+        x += input.x;
+        y -= input.y;
+
+        return new Vector2f(x/scale, y/scale);
+    }
+}
